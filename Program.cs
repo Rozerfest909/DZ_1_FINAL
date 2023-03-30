e1c533e54d81d3c11a32a930f1d2372f64c62d96
a.faron <rozerfest909@gmail.com>
2023-03-30 14:40:31 +0300

    navigate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Branch:
+task_1 - Задача 1
+task_2 - Задача 2
+task_3 - Задача 3
+task_4 - Задача 4
